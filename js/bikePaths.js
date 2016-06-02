fa95b05e0435f475bf18f84826ecf5646355e437
Tim Creasy <timcreasy@me.com>
2016-06-01 21:46:46 -0500

    Everything working, safety commit

--- js/bikePaths.js
+++ js/bikePaths.js
@@ -1,3 +1,58 @@
+function getFiveRacks(data) {
+    
+  var nashvilleCoords = new google.maps.LatLng(36.1627, -86.7816);
+
+
+  for (var i = 0; i < 5; i++) {
+    // Log location name
+    console.log(data[i].detail_loc);
+    // Latitude var
+    var aLat = data[i].lat;
+    //aLat = Number(aLat).toFixed(3);
+    var aLon = data[i].lon;
+    //aLon = Number(aLon).toFixed(3)
+    //var newLatLon = {lat: aLat, lng: aLon};
+    
+    var myLatlng = new google.maps.LatLng(aLat,aLon);
+
+    var mapOptions = {
+      zoom: 12,
+      center: nashvilleCoords
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        title:"Hello World!"
+    });
+
+marker.setMap(map);
+
+    // Create new marker
+    // var marker = new google.maps.Marker({
+    //   position: newLatLon,
+    //   map: map,
+    //   title: "h"
+    // });
+
+  }
+}
+
+function initMap() {
+  var myLatLng = {lat: -25.363, lng: 131.044};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
+
 // Create XMLHttpRequest object
 var xmlhttp = new XMLHttpRequest();
 
@@ -9,7 +64,7 @@ xmlhttp.onreadystatechange = function() {
     // Parse and store responseText
     var data = JSON.parse(xmlhttp.responseText);
     
-    console.log(data);
+    getFiveRacks(data);
   }
 };
 
