dab298f7c9244bf39f45b199e1523f407f148bbd
Tim Creasy <timcreasy@me.com>
2016-06-01 23:03:14 -0500

    Changed title, cleaned comments, improved logic

--- js/bikePaths.js
+++ js/bikePaths.js
@@ -6,13 +6,13 @@ function mapRackLocations(data) {
   // Create Map
   var nashvilleCoords = new google.maps.LatLng(36.1627, -86.7816);
    var mapOptions = {
-      zoom: 12,
+      zoom: 13,
       center: nashvilleCoords
-    }
+  }
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-  // Add each of five markers
-  for (var i = 0; i < 120; i++) {
+  // Cycle through each bike rack location
+  for (var i = 0; i < data.length; i++) {
 
     // Get current racks lat and lon
     var lat = data[i].lat;
@@ -40,21 +40,6 @@ function mapRackLocations(data) {
     marker.setMap(map);
   }
 
-}
-
-function initMap() {
-  var myLatLng = {lat: -25.363, lng: 131.044};
-
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: myLatLng
-  });
-
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
-  });
 }
 
 // Create XMLHttpRequest object
